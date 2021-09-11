5df5c1a6a01a6e0faa80e57839a74042d1fc8019
Su-minn <smsj68@naver.com>
2021-09-11 18:38:46 +0900

    start project

---
+++ journey_memo_jussuit/android/app/src/main/kotlin/com/example/journey_memo_jussuit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.journey_memo_jussuit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ journey_memo_jussuit/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Container(child: Center(child: Text('Journey Memo')))),
+      ),
+    );
+  }
+}
