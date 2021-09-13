e4cc926dc25204519bff31c248bb6711893c1287
Su-minn <smsj68@naver.com>
2021-09-13 19:59:01 +0900

    change theme color (deep champagne)

---
+++ journey_memo_jussuit/lib/constants/material_deep_champagne.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const MaterialColor deep_champagne = MaterialColor(
+  0xFFFFD6A5,
+  <int, Color>{
+    50: Color(0x0FFFD6A5),
+    100: Color(0x1FFFD6A5),
+    200: Color(0x2FFFD6A5),
+    300: Color(0x3FFFD6A5),
+    400: Color(0x4FFFD6A5),
+    500: Color(0x5FFFD6A5),
+    600: Color(0x6FFFD6A5),
+    700: Color(0x7FFFD6A5),
+    800: Color(0x8FFFD6A5),
+    900: Color(0x9FFFD6A5),
+  },
+);
--- journey_memo_jussuit/lib/main.dart
+++ journey_memo_jussuit/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:journey_memo_jussuit/constants/material_deep_champagne.dart';
 import 'package:journey_memo_jussuit/home_page.dart';
 
 void main() => runApp(MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
+      theme: ThemeData(primarySwatch: deep_champagne),
     );
   }
 }
