0ee82a5c287e5ada769683ddeae0e05a683872d2
Su-minn <smsj68@naver.com>
2021-09-15 23:44:39 +0900

    add feedListBuilder

--- journey_memo_jussuit/lib/feed_screen.dart
+++ journey_memo_jussuit/lib/feed_screen.dart
@@ -12,6 +12,14 @@ class FeedScreen extends StatelessWidget {
           style: TextStyle(fontFamily: 'Roblox'),
         ),
       ),
+      body: ListView.builder(itemBuilder: feedListBuilder, itemCount: 30,),
+    );
+  }
+
+  Widget feedListBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.accents[index % Colors.accents.length],
+      height: 200,
     );
   }
 }
